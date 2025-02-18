09bf6ab7bf8a2921e707e27957a4ad3a9641f451
radin-mg <radinart1@gmail.com>
2025-02-19 02:51:54 +0330

    Update Apps.jsx

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -310,7 +310,7 @@ const Apps = ({ subLink }) => {
   useEffect(() => {
     fetch(
       import.meta.env.VITE_JSON_APPS_URL ||
-        "https://raw.githubusercontent.com/MatinDehghanian/public-assets/refs/heads/main/json/os.json"
+        "https://raw.githubusercontent.com/radin-mg/public-assets/refs/heads/main/json/os.json"
     )
       .then((response) => response.json())
       .then((data) => setOperatingSystems(data.operatingSystems));
